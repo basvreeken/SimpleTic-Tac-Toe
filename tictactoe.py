0f7f540539452874a32bf379ce998aac15eed9db
basvreeken <57438379+basvreeken@users.noreply.github.com>
2021-11-01 15:47:41 +0100

    Stage 2/5: The user is the gamemaster

--- tictactoe.py
+++ tictactoe.py
@@ -1,4 +1,24 @@
-# write your code here
-print('''X O X
-O X O
-X X O''')
+# Set the rules
+valid_input = ['X', 'O', '_']
+play = True
+
+# Read a string of 9 symbols
+c = input('Enter cells: ')
+
+if len(c) != 9:
+    print('Input too short')
+    play = False
+
+for x in c:
+    if x not in valid_input:
+        print('Incorrect input')
+        play = False
+        break
+
+# Display the 3x3 grid
+if play is True:
+    print(f'''---------
+| {c[0]} {c[1]} {c[2]} |
+| {c[3]} {c[4]} {c[5]} |
+| {c[6]} {c[7]} {c[8]} |
+---------''')
