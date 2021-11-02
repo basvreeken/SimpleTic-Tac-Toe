294101bc375d6b230526f7aea80a1126f6f5f8c3
basvreeken <57438379+basvreeken@users.noreply.github.com>
2021-11-02 21:24:00 +0100

    Stage 5/5: Fight

--- tictactoe.py
+++ tictactoe.py
@@ -1,77 +1,89 @@
-# Set the rules
-valid_input = ['X', 'O', '_']
-play = True
+finished = False
+grid = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
+turn = 'O'
 
-cells = input('Enter cells: ')
 
-for x in cells:
-    if x not in valid_input:
-        print('Incorrect input')
-        play = False
-
-grid = [cells[0], cells[1], cells[2]], [cells[3], cells[4], cells[5]], [
-    cells[6], cells[7], cells[8]]
-
-# Display the 3x3 grid
-if play is True:
+def show_grid():
     print(f'''---------
 | {grid[0][0]} {grid[0][1]} {grid[0][2]} |
 | {grid[1][0]} {grid[1][1]} {grid[1][2]} |
 | {grid[2][0]} {grid[2][1]} {grid[2][2]} |
 ---------''')
 
-    # Analyze the game state
-    win_lines = []
-    if grid[0][0] == grid[0][1] and grid[0][0] == grid[0][2]:
-        win_lines.append(grid[0][0])
-    if grid[1][0] == grid[1][1] and grid[1][0] == grid[1][2]:
-        win_lines.append(cells[3])
-    if grid[2][0] == grid[2][1] and grid[2][0] == grid[2][2]:
-        win_lines.append(grid[2][0])
-    if grid[0][0] == grid[1][0] and grid[0][0] == grid[2][0]:
-        win_lines.append(grid[0][0])
-    if grid[0][1] == grid[1][1] and grid[0][1] == grid[2][1]:
-        win_lines.append(grid[0][1])
-    if grid[0][2] == grid[1][2] and grid[0][2] == grid[2][2]:
-        win_lines.append(grid[0][2])
-    if grid[0][0] == grid[1][1] and grid[0][0] == grid[2][2]:
-        win_lines.append(grid[0][0])
-    if grid[0][2] == grid[1][1] and grid[0][2] == grid[2][0]:
-        win_lines.append(grid[0][1])
 
-    # Handle players move
+def analyze_state():
+    global finished
+    win_lines = []
+    if grid[0][0] == grid[0][1] and grid[0][0] == grid[0][2] and grid[0][0] \
+            != ' ':
+        win_lines.append(grid[0][0])
+    if grid[1][0] == grid[1][1] and grid[1][0] == grid[1][2] and grid[1][0] \
+            != ' ':
+        win_lines.append(grid[1][0])
+    if grid[2][0] == grid[2][1] and grid[2][0] == grid[2][2] and grid[2][0] \
+            != ' ':
+        win_lines.append(grid[2][0])
+    if grid[0][0] == grid[1][0] and grid[0][0] == grid[2][0] and grid[0][0] \
+            != ' ':
+        win_lines.append(grid[0][0])
+    if grid[0][1] == grid[1][1] and grid[0][1] == grid[2][1] and grid[0][1] \
+            != ' ':
+        win_lines.append(grid[0][1])
+    if grid[0][2] == grid[1][2] and grid[0][2] == grid[2][2] and grid[0][2] \
+            != ' ':
+        win_lines.append(grid[0][2])
+    if grid[0][0] == grid[1][1] and grid[0][0] == grid[2][2] and grid[0][0] \
+            != ' ':
+        win_lines.append(grid[0][0])
+    if grid[0][2] == grid[1][1] and grid[0][2] == grid[2][0] and grid[0][2] \
+            != ' ':
+        win_lines.append(grid[0][2])
+
+    total_x = [cell for row in grid for cell in row].count('X')
+    total_y = [cell for row in grid for cell in row].count('O')
+
+    if abs(total_x - total_y) >= 2:
+        print(total_y)
+        print(total_x)
+        print('Impossible')
+    if 'X' in win_lines and 'O' in win_lines:
+        print('Impossible')
+    elif win_lines:
+        print(win_lines[0] + ' wins')
+        finished = True
+    elif total_x + total_y == 9:
+        print('Draw')
+        finished = True
+
+
+def play(player):
+    global grid
     passed_check = False
-    err_mess = 'You should enter numbers!'
     while not passed_check:
         coords = input('Enter the coordinates: ').split()
         try:
             coords = [int(x) - 1 for x in coords]
-            print(coords)
         except ValueError:
             print('You should enter numbers!')
             continue
         if coords[0] > 2 or coords[1] > 2:
             print('Coordinates should be from 1 to 3!')
             continue
-        elif grid[coords[0]][coords[1]] != '_':
+        elif grid[coords[0]][coords[1]] != ' ':
             print('This cell is occupied! Choose another one!')
             continue
         else:
             passed_check = True
-            grid[coords[0]][coords[1]] = 'X'
-            print(f'''---------
-| {grid[0][0]} {grid[0][1]} {grid[0][2]} |
-| {grid[1][0]} {grid[1][1]} {grid[1][2]} |
-| {grid[2][0]} {grid[2][1]} {grid[2][2]} |
----------''')
+            grid[coords[0]][coords[1]] = player
+            show_grid()
 
-    # if abs(cells.count('X') - cells.count('O')) > 1 or \
-    #         'X' in win_lines and 'O' in win_lines:
-    #     print('Impossible')
-    # elif not win_lines:
-    #     if '_' in cells:
-    #         print('Game not finished')
-    #     else:
-    #         print('Draw')
-    # else:
-    #     print(win_lines[0] + ' wins')
+
+show_grid()
+
+while not finished:
+    if turn == 'X':
+        turn = 'O'
+    else:
+        turn = 'X'
+    play(turn)
+    analyze_state()
